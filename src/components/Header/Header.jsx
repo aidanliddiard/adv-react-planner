3981a6486df21571283b3639337ec08dfe5dde29
aidanliddiard <aidanliddiard@gmail.com>
2022-05-16 16:05:06 -0700

    delete working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
-      {/* TODO: Replace hard-coded count with the actual number of entries */}
       <span>{entries.length} Item Left</span>
     </header>
   );
